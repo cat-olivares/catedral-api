72cd66ac315fab8d099afb4c922519e9d8c62d46
Catalina <catalinaolivaresrozas@gmail.com>
2025-09-17 15:58:13 -0300

    actualizacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot('mongodb+srv://catolivares_admin:di6hWZ7WHpwg5ghW@cluster0.lwpjvjw.mongodb.net/PerfumesCatedral?retryWrites=true&w=majority'),
-    // MongooseModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (config: ConfigService) => ({
-    //     uri: config.get<string>('MONGO_URI'),
-    //   }),
-    // }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+    }),
     UsersModule,
     AuthModule
   ],
