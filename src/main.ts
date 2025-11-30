268d35e35313e1c0749118614dbc6864e51145aa
sgarciavasquez <sebastianalonso7@gmail.com>
2025-11-30 17:26:20 -0300

    Fix start script for Render

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { initFirebase } from './firebase/firebase.init';
 
-const config = new ConfigService();
 async function bootstrap() {
   initFirebase();
 
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    forbidNonWhitelisted: true,
-    validateCustomDecorators: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      validateCustomDecorators: true,
+    }),
+  );
+  const configService = app.get(ConfigService);
 
-  const config = app.get(ConfigService);
-  
-  await app.listen(config.get<number>('PORT', 3000));
+  const port =
+    configService.get<number>('PORT') ||
+    Number(process.env.PORT) ||
+    3000;
+
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
